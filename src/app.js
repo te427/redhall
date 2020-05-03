930258a4c3573d650a9f0c06e48a306d3581179c
Kieran Collery <deathbask3t@gmail.com>
2020-05-03 13:33:18 -0700

    adds 16 bit sprites, an npc dwarf, and text interaction:

--- src/app.js
+++ src/app.js
@@ -1,77 +1,110 @@
 import * as Phaser from 'phaser'
 
-var sprite
+const LEFT = 'l'
+const RIGHT = 'r'
+const UP = 'u'
+const DOWN = 'd'
+
+const dirMap = {
+  [LEFT]: checkLeft,
+  [RIGHT]: checkRight,
+  [UP]: checkUp,
+  [DOWN]: checkDown,
+}
+
+var game
+var scene
+var player
+var dwarf
 var keys
 var animating 
 var footsteps
 var map
 var layer
 var tiles
+var menu 
+var text
+var talking
+var yak
+var dir = DOWN
 
 var config = {
   type: Phaser.AUTO,
-  width: 100,
-  height: 100,
+  width: 16 * 16,
+  height: 16 * 12,
   pixelArt: true,
-  zoom: 8,
+  zoom: 4,
   physics: {
     default: 'arcade',
     arcade: {},
   },
   scene: {
+    key: 'game',
     preload: preload,
     create: create,
     update: update,
   },
 }
 
-var game = new Phaser.Game(config)
+game = new Phaser.Game(config)
 
 function preload() {
-  this.load.spritesheet('player', '../assets/player.png', { frameWidth: 8, frameHeight: 9})
+  this.load.spritesheet('player', '../assets/pc.png', { frameWidth: 16, frameHeight: 16})
+  this.load.spritesheet('dwarf', '../assets/dwarf.png', { frameWidth: 16, frameHeight: 16})
   this.load.audio('footstep', '../assets/footstep.wav')
-  this.load.image('background', '../assets/background.jpg')
-  this.load.tilemapCSV('map', '../data/redhall.csv')
-  this.load.image('tiles', '../assets/tilemap.png')
+  this.load.audio('dwarf', '../assets/dwarf.wav')
+  this.load.tilemapCSV('map', '../assets/map.csv')
+  this.load.image('tiles', '../assets/tiles.png')
+  this.load.image('menu', '../assets/menu.png')
 }
 
 function create() {
+  scene = this
   this.physics.world.setFPS(8)
 
-  map = this.make.tilemap({key: 'map', tileWidth: 8, tileHeight: 8})
+  map = this.make.tilemap({key: 'map', tileWidth: 16, tileHeight: 16})
 
   tiles = map.addTilesetImage('tiles')
   layer = map.createStaticLayer('layer', tiles)
 
-  layer.setCollisionBetween(0, 8);
-  layer.setCollisionBetween(14, 15);
+  layer.setCollisionBetween(0, 14);
 
-  sprite = this.physics.add.sprite(48, 96, 'player')
-  sprite.setCollideWorldBounds(true)
+  player = this.physics.add.sprite(16 * 16, 16 * 16, 'player')
+  player.setCollideWorldBounds(true)
 
-  this.physics.add.collider(sprite, layer)
-  this.physics.world.setBounds(0, 0, 128, 128)
+  this.physics.add.collider(player, layer)
+  this.physics.world.setBounds(0, 0, 100 * 16, 100 * 16)
 
-
+  dwarf = this.physics.add.sprite(24 * 16, 20 * 16, 'dwarf')
+  this.physics.add.collider(player, dwarf)
+  dwarf.setImmovable(true)
 
   this.sound.audioPlayDelay = 0.1;
   this.sound.loopEndOffset = 0.05;
 
   footsteps = this.sound.add('footstep')
+  yak = this.sound.add('dwarf')
 
-  var loopMarker = {
-    name: 'loop',
+  var footstepsMarker = {
+    name: 'walking',
     start: 0,
     duration: 0.25,
     config: {
-        loop: true
+      loop: true
     }
   }
 
-  footsteps.addMarker(loopMarker)
+  var yakMarker = {
+    name: 'yak',
+    start: 0,
+    duration: 2,
+    config: {
+      loop: false 
+    }
+  }
 
-  //background = this.add.tileSprite(0, 0, 2650, 1720, 'background')
-  //background.fixedToCamera = true
+  footsteps.addMarker(footstepsMarker)
+  yak.addMarker(yakMarker)
 
   this.anims.create({
     key: 'walk-down',
@@ -81,14 +114,14 @@ function create() {
   })
 
   this.anims.create({
-    key: 'walk-left',
+    key: 'walk-up',
     repeat: -1,
     frameRate: 8,
     frames: this.anims.generateFrameNames('player', {start: 4, end: 7}) 
   })
 
   this.anims.create({
-    key: 'walk-up',
+    key: 'walk-left',
     repeat: -1,
     frameRate: 8,
     frames: this.anims.generateFrameNames('player', {start: 8, end: 11}) 
@@ -101,61 +134,122 @@ function create() {
     frames: this.anims.generateFrameNames('player', {start: 12, end: 15}) 
   })
 
+  this.anims.create({
+    key: 'stand',
+    repeat: -1,
+    frameRate: 2,
+    frames: this.anims.generateFrameNames('dwarf', {start: 0, end: 1}) 
+  })
+
+  dwarf.play('stand')
+
   keys = this.input.keyboard.createCursorKeys()
 
-  this.cameras.main.setBounds(0, 0, 128, 128)
-  this.cameras.main.startFollow(sprite)
+  this.cameras.main.setBounds(0, 0, 100 * 16, 100 * 16)
+  this.cameras.main.startFollow(player)
+
+  var keyObj = this.input.keyboard.addKey('A')
+
+  keyObj.on('down', interact)
 }
 
 function update() {
   var walking
-  var velocity = 16
+  var velocity = 32
 
   if (keys.space.isDown) {
     velocity *= 2
+    footsteps.setRate(2)
+  } else {
+    footsteps.setRate(1)
   }
 
   if (keys.down.isDown) {
-    sprite.setVelocityY(velocity)
+    player.setVelocityY(velocity)
+    dir = DOWN
     if (!animating) {
-      sprite.anims.play('walk-down')
-      footsteps.play('loop')
+      player.play('walk-down')
+      footsteps.play('walking')
     }
     walking = true
   } 
   
   if (keys.left.isDown) {
-    sprite.setVelocityX(-velocity)
+    player.setVelocityX(-velocity)
+    dir = LEFT
     if (!animating) {
-      sprite.play('walk-left')
-      footsteps.play('loop')
+      player.play('walk-left')
+      footsteps.play('walking')
     }
     walking = true
   }
   
   if (keys.up.isDown) {
-    sprite.setVelocityY(-velocity)
+    player.setVelocityY(-velocity)
+    dir = UP
     if (!animating) {
-      sprite.play('walk-up')
-      footsteps.play('loop')
+      player.play('walk-up')
+      footsteps.play('walking')
     }
     walking = true
   } 
 
   if (keys.right.isDown) {
-    sprite.setVelocityX(velocity)
+    player.setVelocityX(velocity)
+    dir = RIGHT
     if (!animating) {
-      sprite.play('walk-right')
-      footsteps.play('loop')
+      player.play('walk-right')
+      footsteps.play('walking')
     }
     walking = true
   } 
 
   if (!walking) {
-    sprite.setVelocity(0) 
-    sprite.anims.stop()
+    player.setVelocity(0) 
+    player.anims.stop()
     footsteps.stop()
   }
 
   animating = walking
+}
+
+function interact() {
+  const x = player.x
+  const y = player.y
+
+  var canTalk = dirMap[dir](x, y)
+
+  if (canTalk) {
+    if (!talking) {
+      // do this with a different scene
+      menu = scene.physics.add.sprite(8 * 16, (9 * 16) + 8, 'menu')
+      menu.setScrollFactor(0, 0)
+      var topLine = 'Hewwo.. \n\nI am Bwett!!'
+
+      text = scene.add.text(16, 8 * 16, topLine, { font: '"PressStart2P"' });
+      text.setScrollFactor(0, 0)
+      talking = true
+      yak.play('yak')
+    } else  {
+      menu.destroy()
+      text.destroy()
+      talking = false 
+    }
+  }
+}
+
+function checkLeft(x, y) {
+  return dwarf.getBounds().contains(x - 8, y)
+}
+
+function checkRight(x, y) {
+  return dwarf.getBounds().contains(x + 8, y)
+}
+
+function checkUp(x, y) {
+  return dwarf.getBounds().contains(x, y - 8)
+}
+
+function checkDown(x, y) {
+  return dwarf.getBounds().contains(x, y + 8)
 }
--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,19 @@ const path = require('path');
 module.exports = {
   entry: './src/app.js',
   module: {
-    rules: []
+    loaders: [
+      {
+        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+            }
+          }
+        ]
+      }
+    ]
   },
   resolve: {
     extensions: ['.js' ]
